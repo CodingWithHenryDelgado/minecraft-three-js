74cd85cd68e97dc2167872bf915010bb32a6beaa
Henry_DelGlitch <CodingWHenry@gmail.com>
2022-09-22 22:01:53 -0400

    Ability to add cubes/ ability to remove cubes.

--- src/components/Cube.js
+++ src/components/Cube.js
@@ -1,4 +1,5 @@
 import { useBox } from "@react-three/cannon"
+import { useStore } from "../hooks/useStore"
 import * as textures from '../images/textures'
 
 export const Cube = ({ position, texture }) => {
@@ -6,13 +7,49 @@ export const Cube = ({ position, texture }) => {
         type: 'Static',
         position
     }))
+    const [addCube, removeCube] = useStore((state) => [state.addCube, state.removeCube])
 
     const activeTexture = textures[texture + 'Texture']
 
     return (
-        <mesh ref={ref}>
+        <mesh
+            ref={ref}
+            onClick={(e) => {
+                e.stopPropagation()
+                const { x, y, z } = ref.current.position
+                const clickedFace = Math.floor(e.faceIndex / 2)
+                if (e.altKey) {
+                    removeCube(x, y, z)
+                    return
+                }
+                else if (clickedFace === 0) {
+                    addCube(x + 1, y, z)
+                    return
+                }
+                else if (clickedFace === 1) {
+                    addCube(x - 1, y, z)
+                    return
+                }
+                else if (clickedFace === 2) {
+                    addCube(x, y + 1, z)
+                    return
+                }
+                else if (clickedFace === 3) {
+                    addCube(x, y - 1, z)
+                    return
+                }
+                else if (clickedFace === 4) {
+                    addCube(x, y, z + 1)
+                    return
+                }
+                else if (clickedFace === 5) {
+                    addCube(x, y, z - 1)
+                    return
+                }
+            }}
+        >
             <boxBufferGeometry attach="geometry" />
             <meshStandardMaterial map={activeTexture} attach="material" />
-        </mesh>
+        </mesh >
     )
 }
